0b779b1fe1ca34a81f5f96426999e9408d1f5c38
shafisiyam712 <shafisiyam674.com>
2024-10-21 15:22:31 +0600

    first commit

---
+++ src/Utils/addmul.js
@@ -0,0 +1,10 @@
+// const add=(first,second)=>{
+//     return first+second
+// }
+// const mul=(first,second)=>{
+//     return first*second
+// }
+// // export default add
+// // const calculator={add :add}
+// // export default calculator
+// export {add ,mul}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+//import add from "../../Utils/addmul";
+
+// import { add, mul } from "../../Utils/addmul";
+
+
+const Header = () => {
+    // const first =5
+    // const second=5
+    // const sum=add(first,second)
+    // const mul=mul(first,second)
+    return (
+        <div>
+            {/* <h2>Memorable Water Bottle{sum}</h2> */}
+            <h2>Memorable Water Bottle</h2>
+
+        </div>
+    );
+};
+
+export default Header;
